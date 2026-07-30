a5daec46690af9d796ab813aa86952c204754335
Catalina Herazo <catalina.herazo@upb.edu.co>
2026-07-30 15:04:02 -0500

    commit clase 30 julio

---
+++ ejercicio/clase30julio.asm
@@ -0,0 +1,24 @@
+//Creando un código que haga un punto en la pantalla del emulador
+@100
+D=A 
+@PUNT 
+M=D 
+@10
+D=A 
+@CONT 
+M=D 
+(LOOP)
+@7
+D=A 
+@PUNT
+A=M
+M=D 
+@PUNT
+M=M-1
+@CONT
+MD=M-1
+@LOOP
+D;JGT
+(END)
+@END
+0;JMP
